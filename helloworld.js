27b71718fac3b05d3f825da327a11fb859556555
luckystar520 <ychen5@live.maryville.edu>
2021-09-12 15:22:29 -0700

    Update port number to 3333.

--- helloworld.js
+++ helloworld.js
@@ -8,8 +8,8 @@ http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' })
 
     // Send the response body as "Nice to Meet You!"
-    response.end('Nice to Meet You!\n')
-}).listen(3000)
+    response.end('Nice to Meet You on port 3333!\n')
+}).listen(3333)
 
 // Console will print the message
 console.log('Server running')
